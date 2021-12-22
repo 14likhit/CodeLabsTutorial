25c81db98bbbfa2bbddefe78f3f69da7068cfe6f
14likhit <jaijalaram@4495BCU>
2021-12-22 22:47:35 +0530

    - Added Animation to the list elements.

--- app/src/main/java/com/test/codelabstutorial/MainActivity.kt
+++ app/src/main/java/com/test/codelabstutorial/MainActivity.kt
@@ -3,6 +3,9 @@ package com.test.codelabstutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -84,7 +87,13 @@ fun Greeting(name: String) {
         mutableStateOf(false)
     }
 
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        targetValue = if (expanded.value) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         color = MaterialTheme.colors.primary,
@@ -94,7 +103,7 @@ fun Greeting(name: String) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello,")
                 Text(text = name)
